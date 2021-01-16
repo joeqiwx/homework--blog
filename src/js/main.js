2f0752fe88eb389f968c9387283060579b9c6dee
Weixin Qi <s4590288@gmail.com>
2021-01-16 14:48:05 +1000

    fix: 🐛 fix navbar about active and stick funtion
    --active
        change the main.js fix active function, so when click the navbar
    tage it will show the current tag.
    --stike
        when user scroll down the header will be sticker

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,25 @@
 const pages = ['Home', 'Resume', 'Services', 'Blog', 'Contact'];
 showPage = (obj) => {
-    pages.forEach(element => {
-        document.getElementById(element).classList.remove('show');
+    pages.forEach(page => {
+        document.getElementById(page).classList.remove('show');
     });
+
+    if(document.getElementsByClassName('active').length > 0){
+    let actives = [...document.getElementsByClassName('active')] 
+        console.log(document.getElementsByClassName('active'));
+        actives.forEach(active => {
+            active.classList.remove('active');
+        });
+    }
     let page =obj.textContent;
     document.getElementById(page).classList.add('show');
+    obj.classList.add('active');
+}
+const header = document.getElementById('header');
+window.onscroll = () => {
+    setTimeout(() => {
+    window.pageYOffset >= 6 ? header.classList.add('sticker')
+    : header.classList.remove('sticker');
+    }, 100);
+console.log(window.pageYOffset);
 }
